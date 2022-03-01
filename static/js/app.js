df792961eaaf0c8691fce867d5511e0da13faaa4
Kevin Blum <gkmb23@Gmail.com>
2022-03-01 08:51:20 -0600

    11.5.2

--- static/js/app.js
+++ static/js/app.js
@@ -7,4 +7,13 @@ var tbody = d3.select('tbody');
 // Table function build out
 function buildTable(data) {
     tbody.html("");
+
+    data.forEach((datarow) => {
+        let row = tbody.append("tr");
+        Object.values(datarow).forEach((val) =>{
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
 }
