f372cb746797df9d97c61e29cdad65e43a518f5f
Kevin Blum <gkmb23@Gmail.com>
2022-02-28 18:04:09 -0600

    11.5.1

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+// import the data from the data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select('tbody');
+
+// Table function build out
+function buildTable(data) {
+    tbody.html("");
+}
